6a40413715088c9b4dd99323109ef0c207225e7b
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-07-29 10:08:05 +0300

    Update selection_sorting.py

--- selection_sorting.py
+++ selection_sorting.py
@@ -4,7 +4,8 @@ for i in range(n):
   for j in range(i+1, n):
     if (arr[j]<arr[min_pos]):
       min_pos=j #finding the minimum
-      arr[i],arr[min_pos]=arr[min_pos],arr[i]
+  if (min_pos!= i):# inorder to not change if the index and min postion value are in the same index
+    arr[i],arr[min_pos]=arr[min_pos],arr[i]
  return arr
 ##best and worst case both O(N^2)
       
